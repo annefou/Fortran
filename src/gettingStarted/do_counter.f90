cabbfc8f19e25a2a4fdfa086235a7a62684c3e91
Anne Fouilloux <annefou@geo.uio.no>
2015-03-02 13:39:48 +0100

    bug fix
    examples were wrong... we want to print odd number in do_counter and
    do_cycle exit and not the other way around...

--- src/gettingStarted/do_counter.f90
+++ src/gettingStarted/do_counter.f90
@@ -3,11 +3,11 @@ program odd_number
   integer :: N, k
  
 ! odd integers between 0 & N
+  PRINT*, 'Enter an integer N: '
   READ(*,*) N
   WRITE(*,*) "Odd number between 0 and ", N
-  DO k = 0, N, 2
-  WRITE(*,*) k
-END DO
+  DO k = 1, N, 2
+    WRITE(*,*) k, 'is an odd number between 0 and ', N
+  END DO
 
 end program odd_number
-
--- src/gettingStarted/do_cycle_exit.f90
+++ src/gettingStarted/do_cycle_exit.f90
@@ -2,13 +2,14 @@ program odd_number
   implicit none
   integer :: N, k
 ! odd integers between 0 & N
+  PRINT*, 'Enter an integer N'
   READ(*,*) N
   WRITE(*,*) 'Odd number between 0 and ', N
-  k = -1
+  k = 0
   DO 
     k = k + 1
     if (k > N) EXIT
-    if (mod(k,2) .ne. 0) CYCLE
-    WRITE(*,*) k
+    if (mod(k,2) .eq. 0) CYCLE
+    WRITE(*,*) k, ' is an odd number between 0 and ', N
   END DO
 end program odd_number
