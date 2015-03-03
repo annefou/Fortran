0677f0c01e46de1777308e7ba79d8f3dcc808249
Anne Fouilloux <annefou@geo.uio.no>
2015-03-03 16:04:32 +0100

    added a new subroutine to compress a file
    Added a new subroutine to compress a PGM file. And then I had to add a
    new subroutine free_pgm. I want to be able to write and still reuse my
    image for later processing. write_pgm should not be in charge of
    deallocating my pgm image.

--- src/modules/pgm_library.f90
+++ src/modules/pgm_library.f90
@@ -1,39 +1,78 @@
 MODULE pgm_library
+! my module is called pgm_library and
+! my source file is called pgm_library.f90
+! it is not mandatory to do so but it helps
+! when you need to search for a module within 
+! a large set of files
   implicit none
+! try to avoid defining global variable...
 
-  character(len=255)  :: magics, comments
-  integer             :: nx, ny, nmax, err
-
-
-contains
+CONTAINS
 
 subroutine read_pgm(img)
 implicit none
-
+  
   integer,allocatable, intent(out) :: img(:,:)
+  
+  integer             :: nx, ny, nmax, err
 ! read a PGM file
-  read(*,*) magics
-  read(*,*) comments
+! do not need to save what I read as I won't use this information
+  read(*,*) 
+  read(*,*) 
   read(*,*) nx,ny
   allocate(img(nx,ny), STAT=err)
   if (err /= 0) STOP
   read(*,*) nmax
   read(*,*) img
 end subroutine read_pgm
-
+!----------------------------------------
 subroutine write_pgm(img)
 implicit none
-  integer,allocatable, intent(inout) :: img(:,:)
+
+  character(len=2)    :: magics
+  character(len=100)  :: comments
+  integer             :: nx, ny, nmax, err
+  integer, intent(in) :: img(:,:)
   
+  nx = size(img,1)
+  ny = size(img,2)
+  nmax = maxval(img)
 ! write a PGM file  
-  write(*,'(a2)') magics
+  write(*,'("P2")') 
   write(*,'("# file created by test.f90")')
   write(*,*) nx,ny
   write(*,*) nmax
   write(*,*) img
 
-  deallocate(img)
 end subroutine write_pgm
-! ./test.exe < ../../data/moon.pgm  > myfile.pgm 
-
+!----------------------------------------
+subroutine compress_pgm(img,step, small)
+implicit none
+  integer, intent(in)               :: img(:,:)
+  integer, intent(in)               :: step
+  integer, allocatable, intent(out) :: small(:,:)
+  
+  integer                           :: nx,ny, nxs, nys, err
+  
+  ! size of main image
+  nx = size(img,1)
+  ny = size(img,2)
+  ! compute size of the reduced image (small)
+  nxs = size(img(1:nx:step,:),1)
+  nys = size(img(:,1:ny:step),2)
+  
+  allocate(small(nxs,nys), stat=err)
+  if (err /= 0) STOP "Error when allocatin small"
+  
+  small(:,:) = img(1:nx:step,1:ny:step)
+  
+end subroutine compress_pgm
+!----------------------------------------
+subroutine free_pgm(img)
+implicit none
+  integer,allocatable, intent(inout) :: img(:,:)
+  
+  if (allocated(img)) deallocate(img)
+end subroutine free_pgm
 END MODULE pgm_library
+
--- src/modules/test_pgm_main.f90
+++ src/modules/test_pgm_main.f90
@@ -2,7 +2,13 @@ PROGRAM test_pgm_main
   USE  pgm_library
 
   integer,allocatable :: image(:,:)
+  integer,allocatable :: newimage(:,:)
 
  call read_pgm(image)
-call write_pgm(image)
+ write(0,*) 'size of my image: ', size(image,1), size(image,2)
+ call write_pgm(image)
+ call compress_pgm(image,3, newimage)
+ call write_pgm(newimage)
+ call free_pgm(image)
+ call free_pgm(newimage)
 END PROGRAM test_pgm_main
